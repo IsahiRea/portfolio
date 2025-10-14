73eee4f40cf8d7274acb9880434c4db433040809
IsahiRea <Isahi.rea@live.com>
2025-10-14 17:33:16 -0500

    Replaced Images with placeholders

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,3 @@
-import image from '../assets/img/dev-jane-02.jpg'
-
 export default function AboutMe() {
     return (
         <section className="about-me" id="about">
@@ -10,13 +8,13 @@ export default function AboutMe() {
                 {/* TODO: Add a brief introduction */}
                 <p>Hello! My name is Cesar Rea and I enjoy creating things that live on the internet.</p>
                 {/* Talk about interests in helping others bring their designs to life */}
-                <p>I’m passionate about helping others bring their designs to life and creating user-friendly experiences.</p>
-                <p>Here are a few technologies I’ve been working with recently:</p>
+                <p>I'm passionate about helping others bring their designs to life and creating user-friendly experiences.</p>
+                <p>Here are a few technologies I've been working with recently:</p>
                 {/* TODO: Add a list of technologies */}
                 {/* Technologies: Javascript, React, Node.js, Python, SQL */}
             </div>
 
-            <img src={image} alt="Cesar leaning against a bus" className='about-me__img' />
+            <img src="https://placehold.co/400x400/6c63ff/ffffff?text=About+Photo" alt="About me placeholder" className='about-me__img' />
         </section>
     )
 }
